036a1a55ff3bed3c165fb143786b84ad9cb14312
aruntk <aruntknambiar@gmail.com>
2017-12-14 13:11:13 +0530

    compiler v 1.3.10

--- packages/synthesis-compiler/package.js
+++ packages/synthesis-compiler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mwc:synthesis-compiler',
-  version: '1.3.9',
+  version: '1.3.10',
   summary: 'Synthesis is meteor + polymer',
   git: 'https://github.com/meteorwebcomponents/synthesis',
   documentation: 'README.md',
